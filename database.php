c15ed1848faa3388b8b92c63a68122772049716e
Usaila <usailaanjum@gmail.com>
2017-04-04 22:39:50 -0400

    Add a login system

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'auth';
+
+try{
+	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch(PDOException $e){
+	die( "Connection failed: " . $e->getMessage());
+}
+?>
